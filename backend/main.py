ee03dcb9b5d39162064174fc0233d5f92eff3b1c
Lucas Oliveira <lucasoliveiraecomp@gmail.com>
2026-05-22 18:03:28 -0300

    chore: update requeriments.txt

--- backend/main.py
+++ backend/main.py
@@ -35,14 +35,11 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-origins = [
-    "http://localhost:5173",  # Porta padrão do Vite local
-    "http://127.0.0.1:5173",
-]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
