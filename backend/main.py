2d750b41e940774c2fdb3f9822f803dbf2cec71c
Lucas Oliveira <lucasoliveiraecomp@gmail.com>
2026-05-22 07:00:23 -0300

    feat: add CROSS in backend for receive calls from frontend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from backend.api.route import router as menu_router
 from backend.config.database import close_db_client, get_db_client
@@ -26,5 +28,20 @@ app = FastAPI(
     description="API para disponibilizar o cardápio do Restaurante Universitário da UEFS.",
     lifespan=lifespan,
 )
+
+origins = [
+    "http://localhost:5173",  # Porta padrão do Vite local
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="../resources"), name="static")
 
 app.include_router(menu_router)
