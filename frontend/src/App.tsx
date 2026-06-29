2f6ba27a6f0c3da7542059e01927fc8ec756b2a4
Lucas Oliveira <kucasss19@gmail.com>
2026-06-29 10:46:25 -0300

    feat: add vercel Analytics component in main app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { AppRoutes } from "@/routes/AppRoutes";
 import { VacationState } from "@/components/feedback/VacationState";
-
+import { Analytics } from "@vercel/analytics/react";
 const IS_VACATION_MODE = import.meta.env.VITE_IS_VACATION === "true";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <div className="min-h-screen bg-white dark:bg-zinc-950 text-gray-950 dark:text-zinc-50 transition-colors duration-300">
-        {IS_VACATION_MODE ? <VacationState /> : <AppRoutes />}
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <div className="min-h-screen bg-white dark:bg-zinc-950 text-gray-950 dark:text-zinc-50 transition-colors duration-300">
+          {IS_VACATION_MODE ? <VacationState /> : <AppRoutes />}
+        </div>
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 }
