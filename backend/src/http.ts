c906f6db0572a7037efaf3bfcd84ed0b5caa5c34
Lucas Oliveira <kucasss19@gmail.com>
2026-07-13 16:28:37 -0300

    setup: add init from http file and module
    delete init appModule and bootstrap from this module

--- backend/src/http.ts
+++ backend/src/http.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpModule } from './ui/http/http.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(HttpModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
