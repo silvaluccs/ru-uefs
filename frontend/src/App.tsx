c9789f09580c06a55728bcdd72d54e69578c93a2
Lucas Silva <kucasss19@gmail.com>
2026-07-28 20:21:36 -0300

    feat: develop a offline banner when user is off
    was develop a component of feedback
    was create a hook for handle user connection
    was add component in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/contexts/ThemeContext";
 import { AppRoutes } from "@/routes/AppRoutes";
 import { VacationState } from "@/components/feedback/VacationState";
 import { Analytics } from "@vercel/analytics/react";
+import { OfflineBanner } from "@/components/feedback/OfflineBanner";
 const IS_VACATION_MODE = import.meta.env.VITE_IS_VACATION === "true";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <ThemeProvider>
         <div className="min-h-screen bg-white dark:bg-zinc-950 text-gray-950 dark:text-zinc-50 transition-colors duration-300">
           {IS_VACATION_MODE ? <VacationState /> : <AppRoutes />}
+          <OfflineBanner />
         </div>
       </ThemeProvider>
       <Analytics />
